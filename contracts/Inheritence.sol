5058824cb2288500fcefd81d57d65aef80c03cf8
Mir Imad Ahmed <mirimadahmed@outlook.com>
2024-04-04 06:51:29 +0800

    base structure created

---
+++ contracts/Inheritence.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Inheritance {
+    address public owner;
+    address public heir;
+    uint256 public lastWithdrawal;
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+    event HeirChanged(address indexed previousHeir, address indexed newHeir);
+    event Withdrawal(address indexed by, uint256 amount);
+    event FundsReceived(address indexed from, uint256 amount);
+
+    constructor(address _heir) {
+        owner = msg.sender;
+        heir = _heir;
+        lastWithdrawal = block.timestamp;
+    }
+
+    receive() external payable {
+        emit FundsReceived(msg.sender, msg.value);
+    }
+
+    fallback() external payable {
+        emit FundsReceived(msg.sender, msg.value);
+    }
+}
