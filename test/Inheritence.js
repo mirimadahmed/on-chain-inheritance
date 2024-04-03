f258214cbb31470d7e5baba2b0b4763656c1f419
Mir Imad Ahmed <mirimadahmed@outlook.com>
2024-04-04 06:56:20 +0800

    test for heir management

--- test/Inheritence.js
+++ test/Inheritence.js
@@ -94,6 +94,61 @@ describe("Inheritance Smart Contract", function () {
       expect(await inheritance.heir()).to.equal(newHeir.address);
     });
   });
+
+  describe("Heir Management", function () {
+    beforeEach(async function () {
+      const { owner, inheritance } = await loadFixture(deployInheritanceContract);
+
+        // Send some ether to the contract to ensure it has a balance
+        await owner.sendTransaction({ to: inheritance.target, value: parseEther("1.0") });
+        
+      });
+      
+      it("Should allow the heir to designate a new heir", async function () {
+        const { heir, newHeir, inheritance } = await loadFixture(deployInheritanceContract);
+        await time.increase(32 * 24 * 60 * 60);
+        
+        // Heir designates a new heir
+        await inheritance.connect(heir).designateNewHeir(newHeir.address);
+        // Check if the new heir is set correctly
+        expect(await inheritance.heir()).to.equal(newHeir.address);
+    });
+
+    it("Should not allow non-heirs to designate a new heir", async function () {
+      const { newHeir, inheritance } = await loadFixture(deployInheritanceContract);
+      
+        await time.increase(32 * 24 * 60 * 60);
+        // Attempt to designate a new heir from an account that is not the current heir
+        await expect(
+            inheritance.connect(newHeir).designateNewHeir(newHeir.address)
+        ).to.be.revertedWith("Only the heir can call this function.");
+    });
+
+    it("Should not allow setting the current owner or the current heir as the new heir", async function () {
+      const { owner, heir, inheritance } = await loadFixture(deployInheritanceContract);
+
+        await time.increase(32 * 24 * 60 * 60);
+        // Attempt to set the current owner as the new heir
+        await expect(
+            inheritance.connect(heir).designateNewHeir(owner.address)
+        ).to.be.revertedWith("New heir cannot be the current owner.");
+
+        // Attempt to set the current heir as the new heir (to themselves)
+        await expect(
+            inheritance.connect(heir).designateNewHeir(heir.address)
+        ).to.be.revertedWith("New heir cannot be the current heir.");
+    });
+
+    it("Should not allow setting the zero address as the new heir", async function () {
+      const { heir, inheritance } = await loadFixture(deployInheritanceContract);
+
+        await time.increase(32 * 24 * 60 * 60);
+        // Attempt to set the zero address as the new heir
+        await expect(
+            inheritance.connect(heir).designateNewHeir(ethers.ZeroAddress)
+        ).to.be.revertedWith("New heir cannot be the zero address.");
+    });
+  });
 });
 
 
